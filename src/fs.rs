73a4a3c00bf6d80f1ff31e6f01bccaa3cf038af8
Alex J <Fraise@users.noreply.github.com>
2024-04-01 17:42:50 -0400

    Added init, getattr, mkdir and readdir.

---
+++ src/fs.rs
@@ -0,0 +1,189 @@
+use std::ffi::OsStr;
+use std::path::Path;
+use std::time::{Duration, SystemTime};
+use fuser::{FileAttr, Filesystem, FileType, KernelConfig, ReplyAttr, ReplyDirectory, ReplyEntry, Request};
+use libc::{c_int, ENOENT, ENOSYS};
+use rusqlite::Connection;
+use log::debug;
+
+pub struct Fs {
+    pub conn : Connection,
+}
+
+impl Fs {
+    pub fn new() -> Fs {
+        let conn = match Connection::open_in_memory() {
+            Ok(conn) => conn,
+            Err(err) => {
+                println!("error creating the database: {}", err);
+                panic!();
+            }
+        };
+
+        let mut fs = Fs{
+            conn
+        };
+
+        fs
+    }
+}
+
+impl Filesystem for Fs {
+    fn init(&mut self, _req: &Request<'_>, _config: &mut KernelConfig) -> Result<(), c_int> {
+        self.conn.execute(
+            "CREATE TABLE files (
+            inode INTEGER PRIMARY KEY AUTOINCREMENT,
+            parent_inode INTEGER,
+            size INTEGER,
+            path TEXT NOT NULL,
+            file_type TEXT NOT NULL
+        )",
+            (), // empty list of parameters.
+        ).unwrap();
+
+        let f = File {
+            inode: 1,
+            parent_inode: 0,
+            size: 0,
+            path: "/".to_string(),
+            file_type: "dir".to_string(),
+        };
+
+        self.conn.execute("INSERT INTO files (inode, parent_inode, size, path, file_type) VALUES (?1, ?2, ?3, ?4, ?5)", (&f.inode, &f.parent_inode, &f.size, &f.path, &f.file_type)).unwrap();
+
+        Ok(())
+    }
+
+    fn getattr(&mut self, _req: &Request<'_>, ino: u64, reply: ReplyAttr) {
+        println!("getattr: ino={}", ino);
+
+        let mut stmt = self.conn.prepare("SELECT inode, parent_inode, size, path, file_type FROM files WHERE inode=:inode").unwrap();
+        let mut rows = stmt.query_map([ino], |row| {
+            Ok(File {
+                inode: row.get_unwrap(0),
+                parent_inode: row.get_unwrap(1),
+                size: row.get_unwrap(2),
+                path: row.get_unwrap(3),
+                file_type: row.get_unwrap(4),
+            })
+        }).unwrap();
+
+        let f = rows.nth(0).unwrap().unwrap();
+
+        let ts = SystemTime::now();
+        let attr = FileAttr {
+            ino: f.inode,
+            size: f.size,
+            blocks: 0,
+            atime: ts,
+            mtime: ts,
+            ctime: ts,
+            crtime: ts,
+            kind: f.get_file_type(),
+            perm: 0o755,
+            nlink: 0,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+            blksize: 0,
+            flags: 0,
+        };
+        let ttl = Duration::from_secs(1);
+        if ino == 1 {
+            reply.attr(&ttl, &attr);
+        } else {
+            reply.error(ENOSYS);
+        }
+    }
+
+    fn mkdir(&mut self, _req: &Request<'_>, parent: u64, name: &OsStr, mode: u32, umask: u32, reply: ReplyEntry) {
+        println!("mkdir: parent {:?}, name {:?}, mode {:?}, umask {:?}", parent, name, mode, umask);
+        debug!("mkdir: parent {:?}, name {:?}, mode {:?}, umask {:?}", parent, name, mode, umask);
+        let f = File {
+            inode: 0,
+            parent_inode: parent,
+            size: 0,
+            path: name.to_str().unwrap().to_string(),
+            file_type: "dir".to_string(),
+        };
+
+        self.conn.execute("INSERT INTO files (parent_inode, size, path, file_type) VALUES (?1, ?2, ?3, ?4)", (&f.parent_inode, &f.size, &f.path, &f.file_type)).unwrap();
+
+        let ts = SystemTime::now();
+        let attr = FileAttr {
+            ino: f.inode,
+            size: f.size,
+            blocks: 0,
+            atime: ts,
+            mtime: ts,
+            ctime: ts,
+            crtime: ts,
+            kind: f.get_file_type(),
+            perm: 0o755,
+            nlink: 0,
+            uid: 0,
+            gid: 0,
+            rdev: 0,
+            blksize: 0,
+            flags: 0,
+        };
+
+        let ttl = Duration::from_secs(1);
+        //TODO change generation
+        reply.entry(&ttl, &attr, 0);
+    }
+
+    fn readdir(&mut self, _req: &Request<'_>, ino: u64, fh: u64, offset: i64, mut reply: ReplyDirectory) {
+        println!("readdir: ino={}, fh={}, offset={}", ino, fh, offset);
+        debug!("readdir: ino={}, fh={}, offset={}", ino, fh, offset);
+
+        // if offset == 0 {
+        //     reply.add(1, 0, FileType::Directory, &Path::new("."));
+        //     reply.add(1, 0, FileType::Directory, &Path::new(".."));
+        // }
+
+        let mut off = offset;
+
+        let mut stmt = self.conn.prepare("SELECT inode, parent_inode, size, path, file_type FROM files WHERE parent_inode=:inode LIMIT 1000 OFFSET :offset").unwrap();
+        let mut rows = stmt.query_map([ino, offset as u64], |row| {
+            Ok(File {
+                inode: row.get_unwrap(0),
+                parent_inode: row.get_unwrap(1),
+                size: row.get_unwrap(2),
+                path: row.get_unwrap(3),
+                file_type: row.get_unwrap(4),
+            })
+        }).unwrap();
+
+        for file in rows {
+            let f = file.unwrap();
+            let buffer_full = reply.add(f.inode, off, FileType::Directory, f.path);
+            off += 1;
+
+            if buffer_full {
+                println!("reply directory buffer full");
+                break;
+            }
+        }
+
+        reply.ok();
+    }
+}
+
+pub struct File {
+    inode: u64,
+    parent_inode: u64,
+    size: u64,
+    path: String,
+    file_type: String,
+}
+
+impl File {
+    fn get_file_type(&self) -> FileType {
+        return if self.file_type == "dir" {
+            FileType::Directory
+        } else {
+            FileType::RegularFile
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod fs;
+use fs::Fs;
+
+use std::env;
+use fuser::{FileAttr, Filesystem, FileType, ReplyAttr, ReplyDirectory, Request};
+use rusqlite::Connection;
+
+fn main() {
+    env_logger::init();
+
+    let mountpoint = match env::args().nth(1) {
+        Some(path) => path,
+        None => {
+            println!("Usage: {} [mountpoint]", env::args().nth(0).unwrap());
+            return;
+        }
+    };
+
+    let fs = Fs::new();
+
+    match fuser::mount2(fs, mountpoint, &[]) {
+        Ok(_) => {}
+        Err(err) => {
+            println!("error mounting filesystem: {}", err)
+        }
+    }
+}
